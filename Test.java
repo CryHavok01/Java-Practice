93ec48ee9b3af7607ebb87a89c815743265ecfc6
Josh Halpern <jhhalpern@gmail.com>
2021-08-30 14:38:10 -0400

    first java test

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static void main(String[] args) {
+    int age = 35;
+    String firstName = "Josh";
+    String lastName = "Halpern";
+    boolean lookingForWork = true;
+    System.out.println(age + firstName + lastName);
+  }
+}

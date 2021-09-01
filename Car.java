cb47c9c6e6e90c60426ce3f87e192c8965c3219d
Josh Halpern <jhhalpern@gmail.com>
2021-09-01 13:44:07 -0400

    class, constructor, methods, conditionals practice

---
+++ Car.java
@@ -0,0 +1,51 @@
+public class Car {
+  String color;
+  int miles;
+  boolean fast;
+
+  public Car(String paintColor, int milesDriven, boolean isFast) {
+    color = paintColor;
+    miles = milesDriven;
+    fast = isFast;
+  }
+
+  public void drive(int milesDriven) {
+    miles += milesDriven;
+    System.out.println("You have driven " + milesDriven + " miles.  Your total mileage is now " + miles + ".");
+    if (miles > 53000) {
+      System.out.println("It's time for a tune-up");
+    } else {
+      System.out.println("Keep on rolling");
+    }
+  }
+
+  public String repaint(String newColor) {
+    color = newColor;
+    return newColor;
+  }
+
+  public void tuneUp() {
+    fast = true;
+    System.out.println("Vroom vroom");
+  }
+
+  public String toString() {
+    if (!fast) {
+      return "This is a slow " + color + " car that has driven " + miles + " miles.";
+    } else {
+      return "This is a fast " + color + " car that has driven " + miles + " miles.";
+    }
+  }
+  
+  public static void main(String[] args) {
+    Car versa = new Car("white", 52000, false);
+    System.out.println(versa);
+    versa.drive(66);
+    System.out.println("You have repainted your car " + versa.repaint("red"));
+    versa.drive(1000);
+    versa.tuneUp();
+    System.out.println(versa);
+  }
+  
+  
+}
